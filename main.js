3eeeb1a0ed2521fdf8063ead09ba8c5eb847626d
Chuefeng Vue <chuefe.vue@gmail.com>
2020-06-18 13:23:38 -0500

    reinitializing folder

---
+++ main.js
@@ -0,0 +1,14 @@
+const parentElement = document.querySelector('.katas-list')
+
+const sampleArray = [
+    469, 755, 244, 245, 758, 450, 302, 20, 712, 71,
+    456, 21, 398, 339, 882, 848, 179, 535, 940, 472,
+];
+
+// KATA 1
+const kata1Heading = document.createElement('h3')
+kata1Heading.append('KATA 1')
+parentElement.append(kata1Heading)
+
+
+// KATA 2
